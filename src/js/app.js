a32be1780e0bc3bc50616b202f3b60dd7cb6de28
Radostina Emilova <118113591+Radostinaaaa@users.noreply.github.com>
2023-01-17 16:47:52 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,6 @@
-import "../scss/app.scss";
+//import "../scss/app.scss";
+
+
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
@@ -7,4 +9,24 @@ window.addEventListener("DOMContentLoaded", () => {
   button.addEventListener("click", () => {
     alert("💣");
   });
+
+  let elementDiv = document.createElement('div');
+  elementDiv.setAttribute('class','image');
+  let mainElement = document.getElementsByClassName('main');
+  mainElement[0].appendChild(elementDiv);
+  let elementImg = document.createElement('img');
+  elementImg.setAttribute('src','./images/favicon.ico');
+  elementImg.setAttribute('image','');
+  elementDiv.appendChild(elementImg);
+
+ const img = document.querySelector('.image').firstChild;
+ img.addEventListener('click', ()=>{
+  // img.style.transform="scale(2)";
+   elementImg.setAttribute('style','transform: scale(2);');
+ })
+
+
+
 });
+
+
